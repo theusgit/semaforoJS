69bbe8faf04d4c572d36997702c827b81742b4fa
theusgit <theusgit@outlook.com>
2021-01-22 17:26:42 -0300

    Em semaforo.js estou criando o objeto literal recebendo cada cor

--- semaforo.js
+++ semaforo.js
@@ -1,9 +1,14 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 
-const trafficLgth = (event) => {
-    console.log(event.target.id)
+const trafficLigth = (event) => {
+    turnON[event.target.id]();
 }
 
+const turnON = {
+    'red': () => img.src = './img/vermelho.png',
+    'yellow': () => img.src = './img/amarelo.png',
+    'green': () => img.src = './img/verde.png',
+}
 
-buttons.addEventListener('click', trafficLgth);
+buttons.addEventListener('click', trafficLigth);
