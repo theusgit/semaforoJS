efe96eeda0128fb2870c65d703ac955f763a812e
theusgit <theusgit@outlook.com>
2021-01-22 18:51:34 -0300

    adicionando stop

--- semaforo.js
+++ semaforo.js
@@ -1,25 +1,34 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 let colorIndex = 0;
+let intervalId = null;
+
 
 const trafficLigth = (event) => {
+    stopAutomatic();
     turnON[event.target.id]();
+   
 }
 
-const changeColor = () =>{
-    const colors = ['red','yellow','green']
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+
+const changeColor = () => {
+    const colors = ['red', 'yellow', 'green']
     const color = colors[colorIndex];
     turnON[color]();
     nextIndex();
 }
 
-
+const stopAutomatic = () => {
+    clearInterval(intervalId);
+}
 
 const turnON = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => setInterval(changeColor, 1000 )
+    'automatic': () => intervalId = setInterval(changeColor, 2000)
 }
 
 buttons.addEventListener('click', trafficLigth);
